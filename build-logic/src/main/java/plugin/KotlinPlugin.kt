c2031a5545bc766231a4334df8ecffe8425178ed
Won SeungBin <wsb0514@naver.com>
2024-02-17 21:40:31 +0900

    [CHORE] 'KotlinPlugin' 생성

--- build-logic/src/main/java/plugin/KotlinPlugin.kt
+++ build-logic/src/main/java/plugin/KotlinPlugin.kt
@@ -1,2 +1,24 @@
 package plugin
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.PluginManager
+import org.gradle.kotlin.dsl.getByType
+
+internal class KotlinPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        applyPlugin(target.pluginManager)
+        applyJavaPluginExtension(target.extensions.getByType())
+    }
+
+    private fun applyPlugin(manager: PluginManager) = with(manager) {
+        apply("kotlinJvm")
+    }
+
+    private fun applyJavaPluginExtension(extension: JavaPluginExtension) = with(extension) {
+        sourceCompatibility = Build.SOURCE_COMPATIBILITY
+        targetCompatibility = Build.TARGET_COMPATIBILITY
+    }
+
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.hiltAndroid) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.kotlinJvm) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
