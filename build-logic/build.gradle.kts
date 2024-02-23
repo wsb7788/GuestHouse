941ff023abbda5b3ece10dd6a72ced8b2f83f2e3
Won SeungBin <wsb0514@naver.com>
2024-02-23 18:43:04 +0900

    [REFACTOR] build-logic gradle 내부 필요하지 않은 플러그인 생성 구문 주석 삭제

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,16 +7,3 @@ dependencies {
     implementation(libs.android.gradle.plugin)
     implementation(libs.hilt.android.gradle.plugin)
 }
-
-gradlePlugin {
-    /*plugins {
-        register("hilt") {
-            id = "hiltz"
-            implementationClass = "plugin.HiltPlugin"
-        }
-        register("application") {
-            id = "applicationz"
-            implementationClass = "plugin.ApplicationPlugin"
-        }
-    }*/
-}
