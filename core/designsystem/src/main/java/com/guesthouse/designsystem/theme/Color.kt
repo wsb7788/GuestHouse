33d7786055051a53be20657bbd3d620e39197369
Won SeungBin <wsb0514@naver.com>
2024-02-24 17:05:58 +0900

    [CHORE] ColorTheme 설정

---
+++ core/designsystem/src/main/java/com/guesthouse/designsystem/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.guesthouse.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val White70 = Color(0xB2FFFFFF)
+internal val Gray70 = Color(0xB2909090)
+internal val Black70 = Color(0xB2262B2F)
+
+internal val StarLight = Color(0xFFFEE500)
+internal val StarDark = Color(0xFFF2DB08)
+
+internal val Primary = Color(0xFF1B88EC)
+internal val BackGround = Color(0xFFF8F8F8)
