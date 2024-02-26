018bc99f2d709ef7ab2b30aa9725d1a3c43a288e
Won SeungBin <wsb0514@naver.com>
2024-02-26 17:01:29 +0900

    [REFACTOR] .gitignore Icon 포함하도록 수정

---
+++ core/designsystem/src/main/java/com/guesthouse/designsystem/icon/GuestHouseIcons.kt
@@ -0,0 +1,53 @@
+package com.guesthouse.designsystem.icon
+
+import com.guesthouse.designsystem.R
+
+object GuestHouseIcons {
+    val cancel = R.drawable.icon_cancel
+    val aroundMeEnable = R.drawable.icon_around_me_enable
+    val aroundMeFocused = R.drawable.icon_around_me_focused
+    val back = R.drawable.icon_back
+    val calendar = R.drawable.icon_calendar
+    val camera = R.drawable.icon_camera
+    val caution = R.drawable.icon_caution
+    val check = R.drawable.icon_check
+    val checked = R.drawable.icon_checked
+    val crown = R.drawable.icon_crown
+    val delete = R.drawable.icon_delete
+    val emailEntered = R.drawable.icon_email_entered
+    val exitChattingRoom = R.drawable.icon_exit_chatting_room
+    val filter = R.drawable.icon_filter
+    val forward = R.drawable.icon_forward
+    val groupChattingEnable = R.drawable.icon_group_chatting_enable
+    val groupChattingFocused = R.drawable.icon_group_chatting_focused
+    val guestHouseList = R.drawable.icon_guest_house_list
+    val guestHouseRating = R.drawable.icon_guest_house_rating
+    val guestHouseLocation = R.drawable.icon_guest_house_location
+    val hamburger = R.drawable.icon_hamburger
+    val homeEnable = R.drawable.icon_home_enable
+    val homeFocused = R.drawable.icon_home_focused
+    val host = R.drawable.icon_host
+    val invisible = R.drawable.icon_invisible
+    val minusDiable = R.drawable.icon_minus_diable
+    val minusEnable = R.drawable.icon_minus_enable
+    val passwordDefault = R.drawable.icon_password_default
+    val passwordEntered = R.drawable.icon_password_entered
+    val plus = R.drawable.icon_plus
+    val plusDisable = R.drawable.icon_plus_disable
+    val plusEnable = R.drawable.icon_plus_enable
+    val profile = R.drawable.icon_profile
+    val rotate180 = R.drawable.icon_rotate_180
+    val rotateMinus90 = R.drawable.icon_rotate_minus_90
+    val rotatePlus90 = R.drawable.icon_rotate_plus_90
+    val search = R.drawable.icon_search
+    val selectPhoto = R.drawable.icon_select_photo
+    val selectPhotoDefult = R.drawable.icon_select_photo_default
+    val send = R.drawable.icon_send
+    val star = R.drawable.icon_star
+    val tabbarUndefinedEnable = R.drawable.icon_tabbar_undefined_enable
+    val unchecked = R.drawable.icon_unchecked
+    val useCamera = R.drawable.icon_use_camera
+    val userLocation = R.drawable.icon_user_location
+    val visible = R.drawable.icon_visible
+    val selectPhotoEnable = R.drawable.icon_select_photo_enable
+}
