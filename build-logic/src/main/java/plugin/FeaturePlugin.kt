a920f0e343986a5904e75ac23d470d16b1260ee1
YuYangWoo <yuyw0712@naver.com>
2024-03-11 02:51:11 +0900

    [CHORE] featurePlugin 재정의
     - 다른 모듈 implementation 포함

--- build-logic/src/main/java/plugin/FeaturePlugin.kt
+++ build-logic/src/main/java/plugin/FeaturePlugin.kt
@@ -10,17 +10,16 @@ import org.gradle.kotlin.dsl.project
 
 internal class FeaturePlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        with (target) {
+        with(target) {
             configureFeature()
         }
     }
 }
 
-private fun applyDependency(handler: DependencyHandler, libs: VersionCatalog) = with (handler){
+private fun applyDependency(handler: DependencyHandler, libs: VersionCatalog) = with(handler){
     implementation(project(":domain:entity"))
     implementation(project(":domain:usecase"))
 
-    implementation(project(":core:ui"))
     implementation(project(":core:navigation"))
     implementation(project(":core:designsystem"))
 
