f970e2015ad964f60cde4edbc70f79274b810ba4
Won SeungBin <wsb0514@naver.com>
2024-03-28 17:13:33 +0900

    [SETTING] KotlinPlugin.kt `kotlinx-coroutines-core` 추가

--- build-logic/src/main/java/plugin/KotlinPlugin.kt
+++ build-logic/src/main/java/plugin/KotlinPlugin.kt
@@ -1,10 +1,13 @@
 package plugin
 
+import Build
 import extention.getPluginId
 import extention.getVersionCatalog
+import extention.implementation
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.plugins.PluginManager
 import org.gradle.kotlin.dsl.getByType
@@ -19,9 +22,13 @@ internal class KotlinPlugin : Plugin<Project> {
 
 internal fun Project.configureKotlin() {
     applyPlugin(pluginManager, getVersionCatalog())
+    applyDependency(dependencies, getVersionCatalog())
     applyJavaPluginExtension(extensions.getByType())
 }
 
+private fun applyDependency(handler: DependencyHandler, libs: VersionCatalog) = with(handler) {
+    implementation(libs.findLibrary("kotlinx-coroutines-core").get())
+}
 
 private fun applyPlugin(manager: PluginManager, libs: VersionCatalog) = with(manager) {
     apply(libs.getPluginId("kotlin-jvm"))
